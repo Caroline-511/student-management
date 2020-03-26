9fff828f99beb757534ffcc973dffa5512228344
Caroline-511 <caroline99.menezes@gmail.com>
2020-03-26 12:37:01 +0530

    Database connection

--- icon.php
+++ icon.php
@@ -13,7 +13,7 @@ else{
 ?>
 
 <?php
-try{
+/*try{
 	$host="ec2-34-200-101-236.compute-1.amazonaws.com";
 	$dbname="d6i1p71shucj0g";
 	$usr='pngpwmkkjntvqf';
@@ -25,7 +25,17 @@ catch(PgException $e)
 {
 	echo "Error : " .$e->getMessage() . "<br/>";
 	die();
-}
+}*/
+$host="ec2-34-200-101-236.compute-1.amazonaws.com";
+	$dbname="d6i1p71shucj0g";
+	$usr='pngpwmkkjntvqf';
+	$port="5432";
+	$password="38889841dfdd9fb9c512f660ac9adc51072b4170904ac0a6277eb14c018866cb";
+	$conn=pg_connect("host=$host dbname=$dbname user=$usr port=$port password=$password");
+	if (!$conn) {
+ echo "An error occurred.\n";
+ exit;
+	}
 ?>
 
 <?php
