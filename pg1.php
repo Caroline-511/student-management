199564c51a03024e0aa5b4f0f25493f60a7d933c
Caroline-511 <caroline99.menezes@gmail.com>
2020-03-26 11:35:58 +0530

    Database connection

--- pg1.php
+++ pg1.php
@@ -32,19 +32,19 @@ echo "Success";
 ?>
 
 <?php
-try{
+/*try{
 	$conn_string="host=ec2-34-200-101-236.compute-1.amazonaws.com dbname=d6i1p71shucj0g usr=pngpwmkkjntvqf port=5432 password=38889841dfdd9fb9c512f660ac9adc51072b4170904ac0a6277eb14c018866cb";
 	$conn=pg_connect($conn_string);
-}
-/*try
+}*/
+try
 {
 	$host="ec2-34-200-101-236.compute-1.amazonaws.com";
 	$dbname="d6i1p71shucj0g";
 	$usr='pngpwmkkjntvqf';
 	$port="5432";
 	$password="38889841dfdd9fb9c512f660ac9adc51072b4170904ac0a6277eb14c018866cb";
-	$conn=pg_connect("dbname=$dbname");
-}*/
+	$conn=pg_connect("dbname=$dbname host=$host usr=$usr port=$port password=$password");
+}
 catch(PgException $e)
 {
 	echo "Error : " .$e->getMessage() . "<br/>";
