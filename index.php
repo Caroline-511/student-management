d7e86e9f30995eb4104cac394a82e671efffaa33
Caroline-511 <caroline99.menezes@gmail.com>
2020-03-26 12:22:51 +0530

    Database connection

--- index.php
+++ index.php
@@ -46,7 +46,17 @@ if($usn!=''&&$password!='')
 
 
 <?php
-try
+	$host="ec2-34-200-101-236.compute-1.amazonaws.com";
+	$dbname="d6i1p71shucj0g";
+	$usr='pngpwmkkjntvqf';
+	$port="5432";
+	$password="38889841dfdd9fb9c512f660ac9adc51072b4170904ac0a6277eb14c018866cb";
+	$conn=pg_connect("host=$host dbname=$dbname user=$usr port=$port password=$password");
+	if (!$conn) {
+ echo "An error occurred.\n";
+ exit;
+	}
+/*try
 {
 	$host="ec2-34-200-101-236.compute-1.amazonaws.com";
 	$dbname="d6i1p71shucj0g";
@@ -59,14 +69,14 @@ catch(PDOException $e)
 {
 	echo "Error : " .$e->getMessage() . "<br/>";
 	die();
-}
+}*/
 if(isset($_POST['submit']))
 {
      $usn=$_POST['usn'];
      $password=$_POST['psw'];
 if($usn!=''&&$password!='')
  {
-   $query="select * from signup where USN='".$usn."' and Password='".$password."'";
+   $query="select * from signup where usn='".$usn."' and password='".$password."'";
    $result=pg_query($conn,$query);
    //$stmt=$this->pdo->prepare($result);
    //$stmt->execute();
