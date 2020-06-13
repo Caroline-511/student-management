9a71a3606c4fce814c6e0b1180b4d52984fa2d7c
Caroline-511 <caroline99.menezes@gmail.com>
2020-06-13 10:39:09 +0530

    Database connection

--- display.php
+++ display.php
@@ -288,6 +288,7 @@ body {
         <a href="http://guru.nmamit.in" target="_blank">Moodle</a><br/>
         <a href="http://www.nmamit.nitte.edu.in" target="_blank">College Homepage</a><br/>
 		<a href="about.php">About Us</a><br/>
+		<a href="index.php">Logout</a></br>
     </div>
 	</div>
 	</div>
